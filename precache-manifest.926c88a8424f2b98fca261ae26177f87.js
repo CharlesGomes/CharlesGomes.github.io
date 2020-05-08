ada3efd9470df12c8f95c46cf533654ef9a2471a
CharlesGomes <tharlesgomes@hotmail.com>
2020-05-08 09:05:06 -0300

    Primeiro Commit

---
+++ precache-manifest.926c88a8424f2b98fca261ae26177f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4784fab3c0e3915e8b1cc93b8957f463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2d4a3be634e7d3c996",
+    "url": "/static/css/main.fe420859.chunk.css"
+  },
+  {
+    "revision": "1357e9a823c43a816817",
+    "url": "/static/js/2.240a93a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.240a93a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2d4a3be634e7d3c996",
+    "url": "/static/js/main.6ee80eba.chunk.js"
+  },
+  {
+    "revision": "4e806a1857325954481b",
+    "url": "/static/js/runtime-main.5837f40b.js"
+  }
+]);
